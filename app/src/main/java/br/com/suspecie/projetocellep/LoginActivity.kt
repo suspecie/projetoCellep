bfe28bcff87eaf743ce62768caaf4ae96a10c3bd
Su Specie <sspecie13@gmail.com>
2020-07-04 23:00:48 -0300

    Aula 6 - Adicionando shared preferences na tela de login

--- app/src/main/java/br/com/suspecie/projetocellep/LoginActivity.kt
+++ app/src/main/java/br/com/suspecie/projetocellep/LoginActivity.kt
@@ -1,5 +1,6 @@
 package br.com.suspecie.projetocellep
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -16,8 +17,16 @@ class LoginActivity : AppCompatActivity() {
         //clique do botão entrar
         btnLoginEntrar.setOnClickListener {
             //capturando texto digitado pelo usuário nos EditTexts
-            val email = edtEmail.text.toString().trim()
             val password = edtPassword.text.toString().trim()
+            val email = edtEmail.text.toString().trim()
+
+            //recuperando os dados da shared preferences
+            val minhaPreferencia = getSharedPreferences("cadastro-$email", Context.MODE_PRIVATE)
+            val emailCadastradoRecuperado = minhaPreferencia.getString("email", "Chave não encontrada")
+            val passwordCadastradaRecuperado = minhaPreferencia.getString("senha", "Chave não encontrada")
+
+//            System.out.println("email" + emailCadastradoRecuperado)
+
 
             //verificar se existe algum texto digitado
             if (email.isEmpty()) {
@@ -27,11 +36,14 @@ class LoginActivity : AppCompatActivity() {
             } else if (password.isEmpty()) {
                 edtPassword.error = "Campo obrigatório"
                 edtPassword.requestFocus()
-            } else if (email == "suspecie@email.com" && password == "1234") {
+            } else if (email == emailCadastradoRecuperado && password == passwordCadastradaRecuperado) {
                 //apresentando uma mensagem ao usuário
                 Toast.makeText(this@LoginActivity, "Usuário logado com sucesso!", Toast.LENGTH_LONG ).show()
                 //abrir a tela main - primeiro parametro: onde estamos - segundo parametro: para onde iremos
-                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                startActivity(Intent(this@LoginActivity, MainActivity::class.java).apply {
+                    //passagem de parametros de uma tela para outra
+                    putExtra("email", email)
+                })
                 //tirar a tela de login do empilhamento para liberar memória
                 finish()
             } else {
